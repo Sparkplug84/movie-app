f3d34edc13064081836db356045b970fafab244c
Mark McClean <mcclean_mark_02@hotmail.com>
2021-08-01 09:07:46 +0000

    created responsive grid to display movies and added styles to the movie containers

--- src/App.js
+++ src/App.js
@@ -4,17 +4,44 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([
-    // Temporary hard coded movie to test Movie component
+    // Temporary hard coded movies to test Movie component
     {
         "title": "Forrest Gump",
         "rating": 8.5,
         "poster": "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+        "title": "Star Wars",
+        "rating": 8.5,
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    },
+    {
+        "title": "Forrest Gump",
+        "rating": 8.5,
+        "poster": "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+        "title": "Star Wars",
+        "rating": 8.5,
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    },
+    {
+        "title": "Forrest Gump",
+        "rating": 8.5,
+        "poster": "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+        "title": "Star Wars",
+        "rating": 8.5,
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
     }
   ])
 
   return (
     <div className="App">
-      <Movies movies={movies} />
+      <div className="container">
+        <Movies movies={movies} />
+      </div>
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import './Movies.css'
 
 function Movies(props) {
     return (
-        <div>
+        <div className="movies__container">
             {
                 props.movies.map((movie, index) =>(
-                    <div>
-                        <img src={movie.poster} alt="movie image" />
-                        <h1>{movie.title}</h1>
-                        <p>{movie.rating}</p>
+                    <div className="movie">
+                        <img className="movie__image" src={movie.poster} alt="movie image" />
+                        <div className="movie__info">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias at aspernatur nihil dolores iste, exercitationem in officia rem odit ipsam velit quam, maiores quod ducimus laborum voluptatem veritatis tenetur quidem!Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias at aspernatur nihil dolores iste, exercitationem in officia rem odit ipsam velit quam, maiores quod ducimus laborum voluptatem veritatis tenetur</p>
+                        </div>
+                        <div className="movie__details">
+                            <h3>{movie.title}</h3>
+                            <p>{movie.rating}</p>
+                        </div>
                     </div>
                 ))
             }
