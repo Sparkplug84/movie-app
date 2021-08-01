de8f0355371f1f94039c871ee2563660c5681028
Mark McClean <mcclean_mark_02@hotmail.com>
2021-08-01 19:30:45 +0000

    hooked up the API and displayed results from a hard coded search function. Added some additional styles to movie component.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Movies from './components/Movies'
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([
-    // Temporary hard coded movies to test Movie component
-    {
-        "title": "Forrest Gump",
-        "rating": 8.5,
-        "poster": "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
-    },
-    {
-        "title": "Star Wars",
-        "rating": 8.5,
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-    },
-    {
-        "title": "Forrest Gump",
-        "rating": 8.5,
-        "poster": "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
-    },
-    {
-        "title": "Star Wars",
-        "rating": 8.5,
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-    },
-    {
-        "title": "Forrest Gump",
-        "rating": 8.5,
-        "poster": "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
-    },
-    {
-        "title": "Star Wars",
-        "rating": 8.5,
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
-    }
-  ])
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async () => {
+    const MOVIE_API = "https://www.omdbapi.com/?s=back+to+the+future&apikey=aa19cb4"
+    const res = await fetch(MOVIE_API)
+    const data = await res.json()
+    console.log(data)
+    setMovies(data.Search)
+
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
 
   return (
     <div className="App">
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,13 +7,19 @@ function Movies(props) {
             {
                 props.movies.map((movie, index) =>(
                     <div className="movie">
-                        <img className="movie__image" src={movie.poster} alt="movie image" />
+                        <img className="movie__image" src={movie.Poster !== "N/A" ? movie.Poster : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1200px-No-Image-Placeholder.svg.png"} alt={movie.Title} />
                         <div className="movie__info">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias at aspernatur nihil dolores iste, exercitationem in officia rem odit ipsam velit quam, maiores quod ducimus laborum voluptatem veritatis tenetur quidem!Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias at aspernatur nihil dolores iste, exercitationem in officia rem odit ipsam velit quam, maiores quod ducimus laborum voluptatem veritatis tenetur</p>
+                            <p><strong>Movie Title:</strong></p>
+                            <p>{movie.Title}</p>
+                            <p><strong>Release Year:</strong></p>
+                            <p>{movie.Year}</p>
+                            <p><strong>IMDB ID:</strong></p>
+                            <p>{movie.imdbID}</p>
+                            <a href={`https://www.imdb.com/title/${movie.imdbID}/`} target="_blank">View details on <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png"></img></a>
                         </div>
                         <div className="movie__details">
-                            <h3>{movie.title}</h3>
-                            <p>{movie.rating}</p>
+                            <h3>{movie.Title}</h3>
+                            <p>{movie.Rating}</p>
                         </div>
                     </div>
                 ))
